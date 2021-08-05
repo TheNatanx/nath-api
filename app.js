90ad58e3055455fdfd46a5ddad8dc21b7bcea275
Nath <33094967+TheNatanx@users.noreply.github.com>
2021-08-06 01:02:39 +0200

    removed unnecessary dependencies

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 require("dotenv").config();
 
 const express = require('express');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser')
 const router = require('./routes/router');
 
 const app = express();
@@ -14,11 +11,8 @@ const mongoDB = process.env.MONGODB;
 mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => console.log("Successfully connected to MongoDB"));
 const db = mongoose.connection;
 
-app.use(logger('dev'));
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use('/', router);
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
